45f96388f02730b8a4b19e564a6b9bd4618b281e
Algorithm Rae Parsons <algo@envs.net>
2026-08-02 16:21:06 -0700

    README clarifications + hyprland file cleanup

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
 # NixOS Configuration for Laika
 
-My personal NixOS configuration and dotfiles for my laptop named Laika. It uses Hyprland + Noctalia with a focus on general purpose laptop usage: office tasks, browsing, chatting, and programming. It also makes use of flakes as well as Home Manager.
+My personal NixOS configuration and dotfiles for my laptop named Laika. It uses Hyprland + Noctalia with a focus on general purpose laptop usage: office tasks, browsing, chatting, and programming. It also makes use of flakes as well as Home Manager and Hjem.
+
+flake.nix calls
+- config.nix
+- hardware-config.nix
+- home.nix (for Home Manager)
+- hjem.nix (for Hjem)
+- noctalia.nix
+
+home.nix calls
+- dots/alacritty.nix
+- dots/cliphist.nix
+- dots/fish.nix
+- dots/git.nix
+- dots/gtk.nix
+- dots/librewolf.nix
+- dots/mpd.nix
+- dots/mpdscribble.nix
+
+hjem.nix calls
+- dots/fastfetch.jsonc
+- dots/hyprland.lua
+- dots/hyprpaper.conf
+- dots/element.json
 
 # Extra Things Necessary to Configure
 - Syncthing folders
 - Thunar connect to terminal command
  - Edit -> Configure custom actions -> Open Terminal Here -> change command to `alacritty`
 - College wifi (download shell script to connect)
-- Plugin Installers
+- Plugin installers for music production
+- Catppuccin-GTK-Theme install
--- dots/hyprland.lua
+++ dots/hyprland.lua
@@ -1,20 +1,6 @@
--- This is an example Hyprland Lua config file.
--- Refer to the wiki for more information.
--- https://wiki.hypr.land/Configuring/Start/
+-- Laika Laptop hyprland.lua config
 
--- Please note not all available settings / options are set here.
--- For a full list, see the wiki
-
--- You can (and should!!) split this configuration into multiple files
--- Create your files separately and then require them like this:
--- require("myColors")
-
-
-------------------
----- MONITORS ----
-------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- Monitor
 hl.monitor({
     output   = "",
     mode     = "preferred",
@@ -22,176 +8,76 @@ hl.monitor({
     scale    = "1",
 })
 
-
----------------------
----- MY PROGRAMS ----
----------------------
-
--- Set programs that you use
+-- Program name setting
 local terminal    = "alacritty"
 local fileManager = "thunar"
 
--------------------
----- AUTOSTART ----
--------------------
+-- Catppuccin Color Setting
+local pink = "rgb(f5c2e7)"
 
--- See https://wiki.hypr.land/Configuring/Basics/Autostart/
-
+-- Autostart
 hl.on("hyprland.start", function () 
-	hl.exec_cmd("hyprpaper")
+	hl.exec_cmd("hyprpaper") -- loads in quickly
 	hl.exec_cmd("noctalia")
 end)
 
--------------------------------
----- ENVIRONMENT VARIABLES ----
--------------------------------
-
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-
+-- Environment Variables
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 
-
------------------------
------ PERMISSIONS -----
------------------------
-
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
--- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
--- for security reasons
-
--- hl.config({
---   ecosystem = {
---     enforce_permissions = true,
---   },
--- })
-
--- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
--- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
--- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
-
-
------------------------
----- LOOK AND FEEL ----
------------------------
-
--- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
+-- Appearance
 hl.config({
     general = {
-        gaps_in  = 5,
+        gaps_in = 5,
         gaps_out = 5,
-
         border_size = 1,
-
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+            active_border   = { colors = { pink, "rgba(00ff99ee)"}, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
-
-        -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
-        resize_on_border = false,
-
-        -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+        resize_on_border = true,
         allow_tearing = false,
-
         layout = "dwindle",
     },
-
     decoration = {
-        rounding       = 10,
+        rounding = 10,
         rounding_power = 2,
-
-        -- Change transparency of focused and unfocused windows
         active_opacity   = 1.0,
         inactive_opacity = 1.0,
-
         shadow = {
-            enabled      = true,
-            range        = 4,
+        	enabled = true,
+        	range = 4,
             render_power = 3,
-            color        = 0xee1a1a1a,
+            color = 0xee1a1a1a,
         },
-
         blur = {
-            enabled   = true,
-            size      = 3,
-            passes    = 1,
-            vibrancy  = 0.1696,
+            enabled = true,
+            size = 3,
+            passes = 1,
+            vibrancy = 0.1696,
         },
     },
-
-    animations = {
-        enabled = true,
-    },
+    animations = { enabled = true, },
 })
 
--- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
+-- Default Curves and Animations
 hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
 hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
 hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
 hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
 hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+hl.animation({ leaf = "global", enabled = true,  speed = 5,   bezier = "default" })
 
--- Default springs
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
-
-hl.animation({ leaf = "global",        enabled = true,  speed = 5,   bezier = "default" })
-
--- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
--- "Smart gaps" / "No gaps when only"
--- uncomment all if you wish to use that.
--- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
--- hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
--- hl.window_rule({
---     name  = "no-gaps-wtv1",
---     match = { float = false, workspace = "w[tv1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
--- hl.window_rule({
---     name  = "no-gaps-f1",
---     match = { float = false, workspace = "f[1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
-
--- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
-hl.config({
-    dwindle = {
-        preserve_split = true, -- You probably want this
-    },
-})
-
--- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
-hl.config({
-    master = {
-        new_status = "master",
-    },
-})
-
--- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
-hl.config({
-    scrolling = {
-        fullscreen_on_one_column = true,
-    },
-})
-
-----------------
-----  MISC  ----
-----------------
-
+-- Disable Hyprland Defaults
 hl.config({
     misc = {
-        force_default_wallpaper = 1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
+        force_default_wallpaper = 1,
+        disable_hyprland_logo   = true,
     },
 })
 
-
----------------
----- INPUT ----
----------------
-
+-- Inputs
 hl.config({
     input = {
         kb_layout  = "us",
@@ -213,10 +99,7 @@ hl.gesture({
     action = "workspace"
 })
 
----------------------
----- KEYBINDINGS ----
----------------------
-
+-- Hotkeys
 local mainMod = "SUPER"
 local ipc = "noctalia msg "
 
@@ -235,12 +118,10 @@ hl.bind("F9", hl.dsp.exec_cmd("hyprshot -m window -o ~/Pictures/Screenshots"))
 hl.bind("F10", hl.dsp.exec_cmd("hyprshot -m region -o ~/Pictures/Screenshots"))
 hl.bind("ALT + Tab", hl.dsp.exec_cmd(ipc .. "window-switcher"))
 
--- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
 local closeWindowBind = hl.bind(mainMod .. " + K", hl.dsp.window.kill())
--- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))
 
 -- Move focus with mainMod + arrow keys
 hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left" }))
@@ -274,26 +155,14 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
-
---------------------------------
----- WINDOWS AND WORKSPACES ----
---------------------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
--- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
-
--- Example window rules that are useful
-
+-- Default Window Rules
 local suppressMaximizeRule = hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
     name  = "suppress-maximize-events",
     match = { class = ".*" },
     suppress_event = "maximize",
 })
--- suppressMaximizeRule:set_enabled(false)
 
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
     name  = "fix-xwayland-drags",
     match = {
         class      = "^$",
@@ -306,15 +175,6 @@ hl.window_rule({
     no_focus = true,
 })
 
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
-
--- Hyprland-run windowrule
 hl.window_rule({
     name  = "move-hyprland-run",
     match = { class = "hyprland-run" },
@@ -322,6 +182,7 @@ hl.window_rule({
     float = true,
 })
 
+-- Custom Window Rules
 hl.window_rule({ match = { class = "dev.noctalia.Noctalia" }, float = true, })
 
 -- Opacity Rules
@@ -329,8 +190,11 @@ hl.window_rule({ match = { class = "thunar" }, opacity = "0.8",})
 hl.window_rule({ match = { class = "vesktop" }, opacity = "0.9",})
 hl.window_rule({ match = { class = "element" }, opacity = "0.9",})
 hl.window_rule({ match = { class = "REAPER" }, opacity = "0.9",})
+hl.window_rule({match = { class = "youtube-music-desktop-app" }, opacity = "0.9",})
+
+-- Reaper Rules
+hl.window_rule({ match = { class = "REAPER", title = "Confirmation" }, center = true, })
 hl.window_rule({ match = { class = "yabridge-host.exe" }, no_max_size = true, opacity = "1",})
--- maybe incl corner-flung reaper windows be centered, but those with specific locations? no
 
 -- Workspace Rules
 hl.workspace_rule({ workspace = "1", monitor = "", persistent = true })
